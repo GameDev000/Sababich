3881ddd1f4e4bdb81cdd2121cddc684c11951eed
Itamar000Git <itamarbabai98@gmail.com>
2025-12-18 11:41:49 +0200

    finish level 1

---
+++ Assets/Scripts/level1 - israel/UI/Letterbox16by9.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class Letterbox16by9 : MonoBehaviour
+{
+    private const float targetAspect = 16f / 9f;
+    private Camera cam;
+
+    private void Awake()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        float windowAspect = (float)Screen.width / Screen.height;
+        float scaleHeight = windowAspect / targetAspect;
+
+        if (scaleHeight < 1f)
+        {
+            // window is narrower -> add bars top/bottom
+            Rect r = cam.rect;
+            r.width = 1f;
+            r.height = scaleHeight;
+            r.x = 0f;
+            r.y = (1f - scaleHeight) / 2f;
+            cam.rect = r;
+        }
+        else
+        {
+            // window is wider -> add bars left/right
+            float scaleWidth = 1f / scaleHeight;
+            Rect r = cam.rect;
+            r.width = scaleWidth;
+            r.height = 1f;
+            r.x = (1f - scaleWidth) / 2f;
+            r.y = 0f;
+            cam.rect = r;
+        }
+    }
+}
